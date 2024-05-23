fa8b47b5ada592a0cc4867f027ffaafc36036c13
miltiadis <45690339+miltiadiss@users.noreply.github.com>
2024-05-24 00:12:07 +0300

    Update statistic_study_per_participant.py

--- Question 1/statistic_study_per_participant.py
+++ Question 1/statistic_study_per_participant.py
@@ -1,10 +1,17 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Χρησιμοποιήστε μια παλέτα χρωμάτων για συνέπεια στα χρώματα
+colors = sns.color_palette("tab10")  # 10 διαφορετικά χρώματα
 # Ορίστε τον φάκελο που περιέχει τα αρχεία CSV
-path = r"C:\Users\me\PycharmProjects\pythonProject2\harth"
+path = r"/harth"
 output_folder = r"C:\Users\me\PycharmProjects\pythonProject2\participant_statistics"
+# Ορίστε το μονοπάτι του αρχείου στατιστικών
+statistics_summary_file = r"/statistics_summary.csv"
+# Διάβασε το αρχείο στατιστικών
+statistics_df = pd.read_csv(statistics_summary_file)
 os.chdir(path)
 
 # Δημιουργία φακέλου εξόδου αν δεν υπάρχει
@@ -80,10 +87,18 @@ for filename in os.listdir(output_folder):
             mean_value = df[df['Feature'] == column]['Mean'].values[0]
             mean_values[column].append(mean_value)
 
+
 # Δημιουργία γραφήματος
 plt.figure(figsize=(10, 6))
-for column in mean_values.keys():
-    plt.plot(file_names, mean_values[column], label=column)
+for i, (column, color) in enumerate(zip(mean_values.keys(), colors)):
+    plt.plot(file_names, mean_values[column], label=column, color=color)
+
+# Προσθέστε τα σημεία δεδομένων από το "statistics_summary.csv" και χρησιμοποιήστε αντίστοιχα χρώματα
+statistics_mean = statistics_df['Mean'].tolist()
+
+# Προσθέστε οριζόντια γραμμή με αντίστοιχο χρώμα για κάθε γνώρισμα
+for i, (mean, color) in enumerate(zip(statistics_mean, colors)):
+    plt.axhline(y=mean, color=color, linestyle='--', linewidth=1, label=f'Horizontal Line {i+1}')
 
 plt.title('Mean Values of Features Across Participants')
 plt.xlabel('Participants')
@@ -93,3 +108,34 @@ plt.legend()
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+# Λίστα για τα ονόματα των συμμετεχόντων χωρίς την κατάληξη .csv
+file_names = []
+
+# Σχεδιάζουμε τη χρονοσειρά για κάθε αρχείο CSV
+for filename in os.listdir(path):
+    file_path = os.path.join(path, filename)
+    df = pd.read_csv(file_path)
+    # Χωρίς κατάληξη
+    base_name = os.path.splitext(filename)[0]
+
+    # Μετατροπή του timestamp σε datetime
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+    # Δημιουργία γραφήματος για τα γνωρίσματα
+    plt.figure(figsize=(12, 6))
+
+    # Σχεδιάζουμε τη χρονοσειρά για κάθε γνώρισμα
+    for column in ['back_x', 'back_y', 'back_z', 'thigh_x', 'thigh_y', 'thigh_z']:
+        plt.plot(df['timestamp'], df[column], label=column)
+
+    # Διαμόρφωση γραφήματος
+    plt.title(f"Time Series for {base_name}")
+    plt.xlabel("Time")
+    plt.ylabel("Values")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+    # Εμφάνιση γραφήματος
+    plt.show()
